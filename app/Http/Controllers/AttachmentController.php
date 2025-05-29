40d83f5bc92f289f398844fa5b2f1ddef748384c
Chotiwat1825 <53622156+Chotiwat1825@users.noreply.github.com>
2025-05-29 15:07:29 +0700

    update edit Show image

--- app/Http/Controllers/AttachmentController.php
+++ app/Http/Controllers/AttachmentController.php
@@ -6,6 +6,7 @@ use App\Models\PlacementAttachment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Support\Str;
 
 class AttachmentController extends Controller
 {
