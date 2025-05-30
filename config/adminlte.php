2c30c9f109594f0c9633aa9e483cf2a1c244051c
Chotiwat1825 <53622156+Chotiwat1825@users.noreply.github.com>
2025-05-30 19:11:19 +0700

    update logout

--- config/adminlte.php
+++ config/adminlte.php
@@ -403,6 +403,11 @@ return [
             'route' => 'admin.password.edit',
             'icon' => 'fas fa-fw fa-lock',
         ],
+                [
+            'text' => 'ออกระบบ',
+            'url' => 'logout',
+            'icon' => 'fas fa-fw fa-sign-out-alt',
+        ],
         // [
         //     'text' => 'multilevel',
         //     'icon' => 'fas fa-fw fa-share',
